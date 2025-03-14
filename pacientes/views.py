4e448a60b862832691d7c8085c1a435b44a62991
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-03-13 22:49:48 -0500

    html historia clinica

--- pacientes/views.py
+++ pacientes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from django.http import Http404
-from logic.logic_pacientes import get_historia_clinica
+from .logic.logic_pacientes import get_historia_clinica
 
 def consultar_historia_clinica(request, cedula):
     data = get_historia_clinica(cedula)
