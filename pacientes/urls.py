58f80dbd74f5a29eebb2720f17fa1eb03a1ca373
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-04-03 19:55:54 -0500

    arreglo enrutamiento

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import consultar_historia_clinica
+from .views import consultar_historia_clinica, crear_historia_clinica, verificar_paciente
 
 urlpatterns = [
     path("historia-clinica/<str:cedula>/", consultar_historia_clinica, name="historia_clinica"),
+    path('crear-historia-clinica/<str:cedula>/', crear_historia_clinica, name='crear_historia_clinica'),
+    path('api/verificar-paciente/<str:cedula>/', verificar_paciente, name='verificar_paciente'),
 ]
