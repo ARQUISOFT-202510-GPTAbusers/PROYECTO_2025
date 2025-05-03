7fa14fb3314a3a83617ccabb59caf3ac80375157
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-05-03 16:05:29 -0500

    landing page

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -125,12 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static', 'media')
-#MEDIA_URL = '/static/media/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
-)
+]
